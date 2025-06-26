b9d8784a4984bbe26b366d4511415ae27a3946a3
DalineSeritsane <dalinelerato@gmail.com>
2025-06-26 03:45:15 +0200

    store index added

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import ticketReducer from '../features/tickets/ticketSlice';
+
+
+
+
+
+
+export default configureStore({ reducer: { tickets: ticketReducer } });
